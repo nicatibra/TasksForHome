ab2324bd4093744ecdce6ab88eff220354ecfce8
nicatibra <nicatibrahimlibusiness@gmail.com>
2024-10-04 12:30:05 +0400

    Initial Task2

---
+++ Task2 - 04.10.2024/Task2 - 04.10.2024/Program.cs
@@ -0,0 +1,48 @@
+﻿#region SmallestNumberOfArray
+//Ədədlərdən ibarət arrayın ən kiçik elementini tapın
+
+//Həll: Array-in hər bir elementini götürdüyümüz smallest dəyişəni ilə müqayisə etməliyik
+
+//int[] numbersArr = { 3, 4, 1, 2, -3 };
+//int smallest = numbersArr[0];
+
+//for (int i = 0; i < numbersArr.Length; i++)
+//{
+//    if (numbersArr[i] < smallest)
+//    {
+//        smallest = numbersArr[i];
+//    }
+//}
+
+//Console.WriteLine("The smallest number in array = " + smallest);
+
+#endregion
+
+#region SumOfNumbersOfArray
+//Ədədlərdən ibarət arrayın elementleri cemini tapan alqoritm
+
+//int[] numebrsArr = { 1, 2, 3, 4, 5, -2 };
+//int sum = 0;
+//for (int i = 0; i < numebrsArr.Length; i++)
+//{
+//    sum += numebrsArr[i];
+//}
+
+//Console.WriteLine("Sum = " + sum);
+
+#endregion
+
+#region Uzunluq>4
+//Sozlerden ibaret arrayde uzunlugu 4-den boyuk olan sozleri ekrana çap edin
+
+//string[] wordsArr = { "Nicat", "Ibrahimli", "Code", "BackEnd" };
+
+//for (int i = 0; i < wordsArr.Length; i++)
+//{
+//    if (wordsArr[i].Length > 4)
+//    {
+//        Console.WriteLine(wordsArr[i]);
+//    }
+//}
+
+#endregion
